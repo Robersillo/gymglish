92984fa94d9372854b7ad7fdf2c17f35baadfcf1
Robersillo <rober_fcm@hotmail.com>
2020-12-19 14:20:55 +0100

    Cambio completo

--- contacto.php
+++ contacto.php
@@ -30,7 +30,7 @@ if(isset($_POST['submit']))
         strcasecmp($_SESSION['6_letters_code'], $_POST['6_letters_code']) != 0)
     {
         //Note: the captcha code is compared case insensitively.
-        //if you want case sensitive match, update the check above to
+        //if you want case sensitive match, probando, update the check above to
         // strcmp()
         $errors .= "\n La imagen de verificacion no concuerda con el texto";
     }
